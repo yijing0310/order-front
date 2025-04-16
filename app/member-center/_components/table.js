2edfb803c89ba366f4ee60cfce051c0cbb91a856
yijing <n112140@gmail.com>
2025-04-17 00:12:14 +0800

    揪團資料列表初步完成

--- app/member-center/_components/table.js
+++ app/member-center/_components/table.js
@@ -1,7 +1,30 @@
-'use client'
-import React from "react";
-
+"use client";
+import React, { useState, useEffect } from "react";
+import { GROUP_GET } from "@/config/api-path";
+import { useAuth } from "@/context/auth.js";
+import moment from "moment";
 export default function Table() {
+    const { auth, getAuthHeader } = useAuth();
+    const [listData, setListData] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const getFetchGroup = async () => {
+            try {
+                const res = await fetch(GROUP_GET, {
+                    headers: { ...getAuthHeader() },
+                });
+                if (!res.ok) {
+                    throw new Error("請求失敗");
+                }
+                const data = await res.json();
+                setListData(data);
+            } catch (err) {
+                setError("發送請求時發生錯誤:", error);
+            }
+        };
+        getFetchGroup();
+    }, [auth, getAuthHeader]);
     return (
         <>
             <table className="w-full whitespace-nowrap">
@@ -12,6 +35,7 @@ export default function Table() {
                     >
                         <td className="px-2">#</td>
                         <td className="px-3 ">揪團名稱</td>
+                        <td className="px-3 ">餐廳名稱</td>
                         <td className="px-3 ">揪團上限</td>
                         <td className="px-3">結束時間</td>
                         <td className="pl-5 ">狀態</td>
@@ -19,51 +43,44 @@ export default function Table() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr
-                        tabIndex={0}
-                        className="focus:outline-none h-16 border border-gray-100 rounded  "
-                    >
-                        <td className="px-2">1</td>
-                        <td className="px-3">丹丹漢堡</td>
-                        <td className="px-3  ">20</td>
-                        <td className="px-3 text-sm">
-                            2024/4/20 13:00
-                        </td>
-                        <td className="pl-5">
-                            <button className="py-3 px-3 text-sm focus:outline-none leading-none text-red-700 bg-red-100 rounded">
-                                今日截止
-                            </button>
-                        </td>
-                        <td className="pl-4">
-                            <button className="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">
-                                View
-                            </button>
-                        </td>
-                        <td>
-                            <div className="relative px-5 pt-2">
-                                <button
-                                    className="focus:ring-2 rounded-md focus:outline-none"
-                                    onclick="dropdownFunction(this)"
-                                    role="button"
-                                    aria-label="option"
-                                ></button>
-                                <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 hidden">
-                                    <div
-                                        tabIndex={0}
-                                        className="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white"
-                                    >
-                                        <p>Edit</p>
-                                    </div>
-                                    <div
-                                        tabIndex={0}
-                                        className="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white"
-                                    >
-                                        <p>Delete</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
+                    {listData?.data?.map((list, i) => {
+                        return (
+                            <>
+                                <tr
+                                    tabIndex={0}
+                                    className="focus:outline-none h-16 border border-gray-100 rounded  "
+                                >
+                                    <td className="px-2">{i + 1}</td>
+                                    <td className="px-3">{list.title}</td>
+                                    <td className="px-3">{list.restaurant}</td>
+                                    <td className="px-3">{list.max_people}</td>
+                                    <td className="px-3 text-sm">
+                                        {moment(list.deadline).format(
+                                            "YYYY/MM/DD HH:mm"
+                                        )}
+                                    </td>
+                                    <td className="pl-5">
+                                        {list.status == "closed" ? (
+                                            <button className="py-3 px-3 text-sm focus:outline-none leading-none text-red-700 bg-red-100 rounded">
+                                                已截止
+                                            </button>
+                                        ) : list.status == "open" ? (
+                                            <button className="py-3 px-3 text-sm focus:outline-none leading-none text-green-700 bg-green-100 rounded">
+                                                開放中
+                                            </button>
+                                        ) : (
+                                            ""
+                                        )}
+                                    </td>
+                                    <td className="pl-4">
+                                        <button className="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">
+                                            View
+                                        </button>
+                                    </td>
+                                </tr>
+                            </>
+                        );
+                    })}
                 </tbody>
             </table>
         </>
--- config/api-path.js
+++ config/api-path.js
@@ -3,5 +3,7 @@ export const API_SERVER = "http://localhost:3001"
 export const JWT_LOGIN = `${API_SERVER}/login-jwt`
 // 註冊
 export const REGISTER_POST = `${API_SERVER}/admin/register/api`
+// 我的揪團
+export const GROUP_GET = `${API_SERVER}/group/api`
 // 新增揪團 
 export const ADD_GROUP_POST = `${API_SERVER}/group/add/api`
