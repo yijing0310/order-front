fb17ebb22f813e9ee82a9fe5fb758cd79d0e28ab
yijing <n112140@gmail.com>
2025-04-21 00:45:17 +0800

    新增搜尋欄位RWD

--- app/member-center/page.js
+++ app/member-center/page.js
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 import { GROUP_GET } from "@/config/api-path";
 import Select from "./_components/select";
 import Table from "./_components/table";
+import MeberCenterSearch from "./_components/search";
 import { useAuth } from "@/context/auth.js";
 import { useRouter } from "next/navigation";
-
 export default function MemberCenterPage() {
     const { auth, getAuthHeader } = useAuth();
     const [listData, setListData] = useState([]);
@@ -32,7 +32,7 @@ export default function MemberCenterPage() {
             }
         };
         getFetchGroup();
-    }, [auth, getAuthHeader,refresh]);
+    }, [auth, getAuthHeader, refresh]);
     const filteredList = listData?.data?.filter((item) => {
         if (filterStatus === "all") return true;
         return item.status === filterStatus;
@@ -41,30 +41,33 @@ export default function MemberCenterPage() {
     return (
         <>
             <div className="sm:px-6 w-full">
-                <div className="px-4 md:px-10 py-4 md:py-7">
-                    <div className="flex items-center justify-between">
+                <div className="px-4 md:px-10 py-4 md:py-7 mt-5 md:mt-0">
+                    <div className="flex md:flex-row flex-col md:gap-0 gap-3  md:items-center justify-between">
                         <p
                             tabIndex={0}
-                            className="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal"
+                            className="focus:outline-none  text-xl lg:text-2xl font-bold leading-normal"
                         >
                             我的揪團
                         </p>
-                        <div className="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded">
-                            <p>Sort By:</p>
-                            <select
-                                aria-label="select"
-                                className="focus:text-indigo-600 focus:outline-none bg-transparent ml-1"
-                            >
-                                <option className="text-sm text-indigo-800">
-                                    Latest
-                                </option>
-                                <option className="text-sm text-indigo-800">
-                                    Oldest
-                                </option>
-                                <option className="text-sm text-indigo-800">
-                                    Latest
-                                </option>
-                            </select>
+                        <div className=" flex  items-center gap-4 md:gap-0">
+                            <MeberCenterSearch />
+                            <div className="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded  w-3/5">
+                                <p>Sort By:</p>
+                                <select
+                                    aria-label="select"
+                                    className="focus:text-indigo-600 focus:outline-none bg-transparent ml-1"
+                                >
+                                    <option className="text-sm text-indigo-800">
+                                        Latest
+                                    </option>
+                                    <option className="text-sm text-indigo-800">
+                                        Oldest
+                                    </option>
+                                    <option className="text-sm text-indigo-800">
+                                        Latest
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -74,7 +77,10 @@ export default function MemberCenterPage() {
                         filterStatus={filterStatus}
                     />
                     <div className="mt-7 overflow-x-auto ">
-                        <Table filteredList={filteredList} setRefresh={setRefresh}/>
+                        <Table
+                            filteredList={filteredList}
+                            setRefresh={setRefresh}
+                        />
                     </div>
                 </div>
             </div>
